91120d3198d075f738c4bd70ed2f5ec418c60e77
Subhadwip <subhadwipbabai01@gmail.com>
2021-05-11 19:04:43 +0530

    login API

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const app=express()
 app.get("/register",(req,res)=>{
     res.send({message:"Getting the data"})
 })
+app.get('/login',(req,res)=>{
+    res.send({messag:"This is branch number 3"})
+})
 app.listen(3000,()=>{
     console.log("Listening to port 3000")
 })
