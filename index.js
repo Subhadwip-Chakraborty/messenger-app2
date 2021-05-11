9ad6890bfead43bc330e894e9e48391a029c118a
Subhadwip <subhadwipbabai01@gmail.com>
2021-05-11 18:42:31 +0530

    Registration API

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express=require("express")
 const app=express()
 
+app.get("/register",(req,res)=>{
+    res.send({message:"Getting the data"})
+})
 app.listen(3000,()=>{
     console.log("Listening to port 3000")
 })
